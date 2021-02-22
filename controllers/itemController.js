5508e4b7820c0918ed1758c893a47d979c94eb56
Kai Takeshima <takeshimakai@gmail.com>
2021-02-22 13:03:51 -0500

    implement item delete

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -81,17 +81,23 @@ exports.itemCreatePost = [
 
 // Display item delete form on GET
 exports.itemDeleteGet = (req, res, next) => {
-  res.send('Not yet implemented.');
+  Item.findById(req.params.id).exec(function(err, item) {
+    if (err) return next(err);
+    res.render('itemDelete', { title: `Delete Item: ${item.title}`, item });
+  });
 };
 
 // Handle item delete form on POST
 exports.itemDeletePost = (req, res, next) => {
-  res.send('Not yet implemented.');
+  Item.findByIdAndRemove(req.body.itemid, function deleteItem(err) {
+    if (err) return next(err);
+    res.redirect('/item');
+  })
 };
 
 // Display item edit form on GET
 exports.itemEditGet = (req, res, next) => {
-  res.send('Not yet implemented.');
+  res.render()
 };
 
 // Handle item edit form on POST
